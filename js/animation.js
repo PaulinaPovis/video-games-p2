f5a0cec1aeb03333126d257ba2e3ea543a4f63a8
Esther <sther37.92@gmail.com>
2022-11-12 17:25:14 +0100

    animaciones mostrar en home no van

---
+++ js/animation.js
@@ -0,0 +1,18 @@
+/*const animado = document.querySelectorAll("#animado");
+    
+setTimeout(function() {
+    // Do something after 2 seconds
+    
+    animado.removeClass("content-hidden")
+}, 2000);
+
+
+
+
+const animationBall = document.getElementById('#animatedBall');
+    document.ready(=>{
+        animationBall.animate({
+            right: '+=500px',
+        })
+      })
+*/
